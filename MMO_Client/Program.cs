17d42cc279792d6b11cddf859f4d3e68443473a9
gmcghie <glen@mcghie.org>
2018-04-10 07:42:29 -0700

    Skeleton setup

---
+++ MMO_Client/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MMO_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server Starting...");
+
+
+
+            Console.WriteLine("Server Shutdown.");
+        }
+    }
+}
---
+++ MMO_Server/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MMO_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Client Starting...");
+
+
+
+            Console.WriteLine("Client Shutdown.");
+        }
+    }
+};
